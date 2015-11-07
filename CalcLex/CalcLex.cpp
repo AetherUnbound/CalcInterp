51b3ec7b4eb4cf829eec91901d79c1cb703b5a09
Matthew Bowden <bowdenm@spu.edu>
2015-11-07 14:57:20 -0800

    Allow file read and exception handling
    Implemented a function to read in file given file (through command line
    arguments). Also put try/catch block around yylex function for proper
    exception handling. Added tokenCount global to track total number of
    tokens.

--- CalcLex/CalcLex.cpp
+++ CalcLex/CalcLex.cpp
@@ -11,20 +11,65 @@ using namespace std;
 string inputFile;
 string::iterator currPos;
 bool endOfFile = false;
+int tokenCount = 0;
 
 Token yylex(); //might be global variables here
+void convertFiletoString(string);
 
 int main(int argc, char *argv[]) {
+	setupMap(); //sets up the map to pring ENUM names
 	
-	inputFile = "read A jeff := 2 + 4 write jeff /* comment * ** */ \n /* \n read B /**/ B := \t 2 / 5 $$";
+
+	//Get the filename from the command line 
+	if (argc > 2) {
+		cerr << "Usage: " << argv[0] << " FILENAME" << endl;
+		return 1;
+	}
+	else if (argc == 1) {
+		//==DEBUG TESTING==
+		inputFile = "read A jeff_224 := 2 + 4 Write jeff /* comment * ** */ \n /* \n rEAd B /**/ B := \t 2 / 5 qq22qq:=1+1.5975/2 ";
+		inputFile += "sum := A + B write sum $$";
+	}
+	else {
+		string fileName = argv[1];
+		convertFiletoString(fileName);
+		cout << inputFile << endl;
+	}
 	currPos = inputFile.begin();
 
 	while (!endOfFile) {
-		yylex();
+		try {
+			yylex();
+		} 
+		catch (exception& e) 
+		{
+			cout << e.what() << endl;
+			cout << "==Halting Program==" << endl;
+			return 0;
+		}
 	}
-
-
+	cout << endl << "Number of tokens: " << tokenCount << endl;
 	return 0;
+}
+
+//Function to convert the file specified in the command line
+//into the string to be iterated
+void convertFiletoString(string fileName) {
+	ifstream fileToOpen(fileName);
+	if (fileToOpen.is_open())
+	{
+		inputFile = ""; //initialise input file
+		string line;
+		while (fileToOpen.good())
+		{
+			getline(fileToOpen, line);
+			inputFile += line;
+			//because getline discards delimiters, need to add it back in here
+			inputFile += '\n'; 
+		}
+		fileToOpen.close();
+	}
+	else cout << "File Open error" << endl;
 }
 
 Token yylex() {
@@ -205,6 +250,7 @@ Token yylex() {
 			}		
 		}
 	} while (!tokenFound);
-	cout << "Tokens: " << thisToken.TokenNum << " " << thisToken.TokenString << endl;
+	cout << "Token: " << setfill('0') << setw(2) << thisToken.TokenNum << " " << TOKENMAP[thisToken.TokenNum] << " (" << thisToken.TokenString << ")" << endl;
+	tokenCount++;
 	return thisToken;
 }
--- CalcLex/calcLex.h
+++ CalcLex/calcLex.h
@@ -7,28 +7,48 @@
 #include <iomanip>
 #include <stdexcept>
 #include <cctype>
+#include <map>
 
 using namespace std;
 
 enum CALCTOKENS {
 	EOFSY = 0, //End of file
-	ID = 1, //ID
-	NUMCONST = 2, //number constant
-	READYSY = 3, //read keyword
-	WRITESY = 4, //write keyword
-	ASSIGNSY = 5, //assignment symbol ':=' 
-	LPAREN = 6, //left parenthesis '('
-	RPAREN = 7, //right parenthesis ')'
-	ADDOP = 8, //addition sign '+'
-	MINOP = 9, //minus sign '-'
-	MULTOP = 10, //multiplication sign '*'
-	DIVOP = 11, //division sign '/'
+	ID, //ID
+	NUMCONST, //number constant
+	READYSY, //read keyword
+	WRITESY, //write keyword
+	ASSIGNSY, //assignment symbol ':=' 
+	LPAREN, //left parenthesis '('
+	RPAREN, //right parenthesis ')'
+	ADDOP, //addition sign '+'
+	MINOP, //minus sign '-'
+	MULTOP, //multiplication sign '*'
+	DIVOP, //division sign '/'
 	/*DOT = 12, //dot symbol '.'
 	//don't know that I need these 
 	RCOMM = 13, //right comment symbol '/*'
 	LCOMM = 14, //left comment symbol '*/
 	
 };
+
+map<CALCTOKENS, string> TOKENMAP;
+
+void setupMap () {
+	TOKENMAP[EOFSY] = "EOFSY";
+	TOKENMAP[ID] = "ID";
+	TOKENMAP[NUMCONST] = "NUMCONST";
+	TOKENMAP[READYSY] = "READYSY";
+	TOKENMAP[WRITESY] = "WRITESY";
+	TOKENMAP[ASSIGNSY] = "ASSIGNSY";
+	TOKENMAP[LPAREN] = "LPAREN";
+	TOKENMAP[RPAREN] = "RPAREN";
+	TOKENMAP[ADDOP] = "ADDOP";
+	TOKENMAP[MINOP] = "MINOP";
+	TOKENMAP[MULTOP] = "MULTOP";
+	TOKENMAP[DIVOP] = "DIVOP";
+};
+
+
 
 struct Token {
 	CALCTOKENS TokenNum;
